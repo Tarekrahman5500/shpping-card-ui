7a6b595bc1b6b6719d3dd21b463b6c00f4070129
tr617 <tr617739@gmail.com>
2023-03-01 22:53:41 +0600

    login register and single product and cart item design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Home from "./pages/Home.jsx";
 import ProductList from "./pages/productList.jsx";
+import Product from "./pages/product.jsx";
+import Register from "./pages/register.jsx";
+import Login from "./pages/login.jsx";
+import Cart from "./pages/cart.jsx";
 
 function App() {
     return (
         <div>
-           <ProductList/>
+           <Cart/>
         </div>
     )
 }
